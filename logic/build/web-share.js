ebf9f7b09ffd8941db601e47a1f2dc9b75a16ad1
coddedwells <wells.mail.13@gmail.com>
2023-02-08 15:30:50 +0300

    📦 NEW: done ui

--- logic/build/web-share.js
+++ logic/build/web-share.js
@@ -1,2 +1,13 @@
 "use strict";
-console.log('hello world');
+const facebook = document.getElementById('facebook');
+const linkedin = document.getElementById('linkedIn');
+const twitter = document.getElementById('twitter');
+const handleShare = (e) => {
+    if (e) {
+        let target = e.target;
+        console.log(target.id);
+    }
+};
+facebook === null || facebook === void 0 ? void 0 : facebook.addEventListener('click', handleShare);
+linkedin === null || linkedin === void 0 ? void 0 : linkedin.addEventListener('click', handleShare);
+twitter === null || twitter === void 0 ? void 0 : twitter.addEventListener('click', handleShare);
--- src/web-share.ts
+++ src/web-share.ts
@@ -1,1 +1,16 @@
-console.log('hello world')
+const facebook: HTMLElement | null = document.getElementById('facebook');
+const linkedin: HTMLElement | null = document.getElementById('linkedIn');
+const twitter: HTMLElement | null = document.getElementById('twitter');
+
+const handleShare = (e: MouseEvent): void => {
+	if (e) {
+        let target = e.target as  HTMLElement
+		console.log(target.id)
+	}
+};
+
+
+facebook?.addEventListener('click', handleShare);
+linkedin?.addEventListener('click', handleShare);
+twitter?.addEventListener('click', handleShare);
+
