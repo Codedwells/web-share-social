a51995229ab756ef309faec5fbbd3d38bea52e31
coddedwells <wells.mail.13@gmail.com>
2023-02-08 10:38:22 +0300

    📦 NEW: initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./**/*.{html,js}'],
+  darkMode: 'class',
+  theme: {
+      extend: {
+          fontFamily: {
+              Raleway: ['"Raleway"', 'sans-serif'],
+              Ubuntu: ['Ubuntu', 'sans-serif'],
+              sans: ['Poppins', 'sans-serif']
+          }
+      }
+  },
+ 
+};
